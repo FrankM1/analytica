e79b4df4d88f9095eb529b181423d202e3a1b9ca
Franklin Gitonga <frank@radiumthemes.com>
2018-07-24 15:47:13 +0300

    removed old doc types

--- includes/structure/general/general-header.php
+++ includes/structure/general/general-header.php
@@ -18,11 +18,8 @@ add_action( 'analytica_doctype', 'analytica_do_doctype' );
  * @since 1.0.0
  */
 function analytica_do_doctype() {
-    ?><!DOCTYPE html>
-    <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
-    <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
-    <!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
-    <!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
 <head><?php
 }
 
