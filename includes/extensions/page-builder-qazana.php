25d7b8effaa1046312588c0b26741de76fdbf550
frank.gitonga <frank@radiumthemes.com>
2019-02-06 16:14:05 +0300

    qazana override site footer

--- includes/extensions/page-builder-qazana.php
+++ includes/extensions/page-builder-qazana.php
@@ -37,7 +37,8 @@ class Qazana {
         add_filter( 'qazana/schemes/default_color_picker_schemes', [ $this, 'default_color_picker_schemes'] );
         add_filter( 'qazana/schemes/system_color_schemes', [ $this, 'system_color_schemes'] );
         add_filter( 'qazana/schemes/default_color_schemes', [ $this, 'default_colors'] );
-        add_filter( 'qazana/schemes/default_fonts', [ $this, 'reset_default_font'] );
+		add_filter( 'qazana/schemes/default_fonts', [ $this, 'reset_default_font'] );
+		add_filter( 'qazana/footers/get_injection_hook', [ $this, 'site_footer_support' ] );
     }
 
     /**
@@ -64,7 +65,7 @@ class Qazana {
 
 	/**
      * Add page selector to qazana
-     * 
+     *
      * @return string css selector
      */
     function page_selector( $base_selector, $model ) {
@@ -73,7 +74,7 @@ class Qazana {
 
     /**
      * Add hero title selector to qazana
-     * 
+     *
      * @return string css selector
      */
     function page_title_selector(){
@@ -260,5 +261,16 @@ class Qazana {
         }
 
         return $retval;
-    }
+	}
+
+	/**
+	 * Site Footer
+	 *
+     * @since 1.0.0
+     *
+     * @return boolean
+     */
+	public function site_footer_support( $option ) {
+		return 'analytica_footer';
+	}
 }
