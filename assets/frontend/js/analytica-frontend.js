1baceb85f6ad185128862bd4e19bfe2b3bdf9034
Franklin Gitonga <1177638+FrankM1@users.noreply.github.com>
2020-11-28 01:02:31 +0300

    mobile menu bug fix

--- assets/frontend/js/analytica-frontend.js
+++ assets/frontend/js/analytica-frontend.js
@@ -1,12 +1,12 @@
-jQuery( document ).ready( function() {
-    jQuery( 'html' ).removeClass( 'no-js' );
-    jQuery( window ).on( 'scroll', function() {
-        if ( window.pageYOffset >= 100 ) {
-            jQuery( 'body' ).addClass( 'page-scrolling' );
-        } else {
-            jQuery( 'body' ).removeClass( 'page-scrolling' );
-        }
-    });
+jQuery(document).ready(function () {
+	jQuery("html").removeClass("no-js");
+	jQuery(window).on("scroll", function () {
+		if (window.pageYOffset >= 100) {
+			jQuery("body").addClass("page-scrolling");
+		} else {
+			jQuery("body").removeClass("page-scrolling");
+		}
+	});
 });
 
 /**
@@ -19,29 +19,41 @@ jQuery( document ).ready( function() {
  * @package Analytica
  */
 
-( function() {
-	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' ) > -1,
-	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' ) > -1;
+(function () {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf("webkit") > -1,
+		is_opera = navigator.userAgent.toLowerCase().indexOf("opera") > -1,
+		is_ie = navigator.userAgent.toLowerCase().indexOf("msie") > -1;
 
-	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
-		window.addEventListener( 'hashchange', function() {
-			var id = location.hash.substring( 1 ),
-				element;
+	if (
+		(is_webkit || is_opera || is_ie) &&
+		document.getElementById &&
+		window.addEventListener
+	) {
+		window.addEventListener(
+			"hashchange",
+			function () {
+				var id = location.hash.substring(1),
+					element;
 
-			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
-				return;
-			}
-
-			element = document.getElementById( id );
-
-			if ( element ) {
-				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
-					element.tabIndex = -1;
+				if (!/^[A-z0-9_-]+$/.test(id)) {
+					return;
 				}
 
-				element.focus();
-			}
-		}, false );
+				element = document.getElementById(id);
+
+				if (element) {
+					if (
+						!/^(?:a|select|input|button|textarea)$/i.test(
+							element.tagName
+						)
+					) {
+						element.tabIndex = -1;
+					}
+
+					element.focus();
+				}
+			},
+			false
+		);
 	}
-}() );
+})();
--- assets/frontend/js/modules/general.js
+++ assets/frontend/js/modules/general.js
@@ -1,10 +1,10 @@
-jQuery( document ).ready( function() {
-    jQuery( 'html' ).removeClass( 'no-js' );
-    jQuery( window ).on( 'scroll', function() {
-        if ( window.pageYOffset >= 100 ) {
-            jQuery( 'body' ).addClass( 'page-scrolling' );
-        } else {
-            jQuery( 'body' ).removeClass( 'page-scrolling' );
-        }
-    });
+jQuery(document).ready(function () {
+	jQuery("html").removeClass("no-js");
+	jQuery(window).on("scroll", function () {
+		if (window.pageYOffset >= 100) {
+			jQuery("body").addClass("page-scrolling");
+		} else {
+			jQuery("body").removeClass("page-scrolling");
+		}
+	});
 });
